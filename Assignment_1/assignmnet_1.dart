4fec7cdc7339c0f789a7c591e8f59b82bb433539
Mehadi Hasan Reaz <reazmehadihasan@gmail.com>
2023-04-15 00:47:38 +0600

    first commit

---
+++ Assignment_1/assignmnet_1.dart
@@ -0,0 +1,12 @@
+main() {
+  List phoneNumber = [
+    '+88',
+    '01768131685',
+    '01768171985',
+    '01768111286',
+    '01768131685'
+  ];
+  for (var i = 1; i <= 4; i++) {
+    print(phoneNumber[0] + phoneNumber[i]);
+  }
+}
