a4c83771ab406a47b02dbfa12733df1617349a5f
Mehadi Hasan Reaz <65062761+MehadiReaz@users.noreply.github.com>
2023-05-08 20:01:42 +0600

    Create assignment_2.dart

---
+++ Assignment_1/assignment_2.dart
@@ -0,0 +1,24 @@
+String studentGrade(String studentName, int testScore) {
+  if (testScore < 0 || testScore > 100) {
+    return "Invalid Grade";
+  } else if (testScore >= 90 && testScore <= 100) {
+    return "A";
+  } else if (testScore >= 80 && testScore <= 89) {
+    return "B";
+  } else if (testScore >= 70 && testScore <= 79) {
+    return "C";
+  } else if (testScore >= 60 && testScore <= 69) {
+    return "D";
+  } else if (testScore >= 0 && testScore <= 59) {
+    return "F";
+  } else {
+    return "Invalid Grade";
+  }
+}
+
+void main() {
+  String studentName = "Mehadi Hasan Reaz";
+  int testScore = 90;
+  String grade = studentGrade(studentName, testScore);
+  print("$studentName's Grade: $grade");
+}
